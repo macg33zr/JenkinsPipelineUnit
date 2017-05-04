4a1b3d4dd2fb80b8430aa4cdd16bf79c6faab172
ogu <Ozan.Gunalp@lesfurets.com>
2017-05-04 14:57:57 +0200

    try to reproduce #28

---
+++ src/test/resources/libs/commons@feature/vars/greetings.groovy
@@ -0,0 +1,9 @@
+// file: vars/greetings.groovy
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    sayHello {}
+}
---
+++ src/test/resources/libs/commons@feature/vars/sayHello.groovy
@@ -0,0 +1,9 @@
+// file: vars/sayHello.groovy
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    echo 'say Hello!'
+}
